50082da6bbfb9c5bbad52f52f30fac3d009ef548
leonardo Kaplan <kaplanleonardo@gmail.com>
2020-06-04 19:49:29 -0500

    adding preview images for hover feature in green tabs

--- site_scripts/profiles_scripts.js
+++ site_scripts/profiles_scripts.js
@@ -1,5 +1,5 @@
 
-alert("hello?")
+
 
 let tabLayer   = document.getElementsByClassName('tab-layer');
 let blueTab    = tabLayer[0];
@@ -7,21 +7,18 @@ let greenTab   = tabLayer[1];
 let redTab     = tabLayer[2];
 let homeTab    = document.getElementsByTagName('header')[0];
 
-homeTab.onclick = function(){
-  // window.open ('../index.html','_self',false)
-}
 
 blueTab.onclick = function(){
   console.log("blue");
-  // window.open ('../index.html','_self',false)
+  window.open ('../index.html','_self',false)
 }
 
 greenTab.addEventListener('click',function(){
   console.log("green tab ");
-  // window.open ('../index.html','_self',false)
+  window.open ('../index.html','_self',false)
 });
 
 redTab.addEventListener('click',function(){
   console.log("red tab ");
-  // window.open ('../index.html','_self',false)
+  window.open ('../index.html','_self',false)
 })
